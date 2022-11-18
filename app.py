51b6a9fe725891b81b11ca25c9cc5d8d5570b4e1
atingupta2005 <atingupta2005@gmail.com>
2022-11-18 16:23:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Flask inside Docker by Atin!!"
+    return "Flask inside Docker by Atin Gupta - v1!!"
 
 
 if __name__ == "__main__":
